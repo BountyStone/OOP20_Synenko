682177b9fa578d7d80b2e63c0a8b1d286394fd3b
boyni <boyni@BOUNTY>
2023-06-10 13:23:39 +0300

    Add project files.

---
+++ OOP20_Synenko/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static double CalculateTrianglePerimeter(double a, double b, double c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0)
+        {
+            throw new ArgumentException("Довжина сторони трикутника повинна бути додатнім числом.");
+        }
+
+        double perimeter = a + b + c;
+        return perimeter;
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            double sideA = 5;
+            double sideB = 4;
+            double sideC = 3;
+
+            double perimeter = CalculateTrianglePerimeter(sideA, sideB, sideC);
+
+            Console.WriteLine("Периметр трикутника: " + perimeter);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Помилка: " + ex.Message);
+        }
+
+        Console.ReadLine();
+    }
+}
